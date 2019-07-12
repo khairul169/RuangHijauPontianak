c069aa9f22e0962326a7b86d26fbf3221ec673bb
Khairul Hidayat <contact@khairul.my.id>
2019-07-12 10:33:01 +0700

    test flatlist onUpdate and onEndReached

--- sources/feeds.js
+++ sources/feeds.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View, ScrollView, TouchableOpacity } from 'react-native'
+import { Text, StyleSheet, View, FlatList } from 'react-native'
 import { Header, PhotoListItem, HeaderButton } from './components'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 export default class Feeds extends Component {
+	state = {
+		refreshing: false,
+		data: [1, 2, 3, 4]
+	}
+	
 	render() {
 		return (
 			<View style={styles.container}>
 				<Header title="Hijau Kota Kita" centerTitle
 					leftComponent={
-						<FontAwesome name='comments' style={{color: '#689F38'}} size={20} />
+						<HeaderButton onPress={() => this._feedList.scrollToOffset({offset: 0})} component={
+								<FontAwesome name='comments' style={{color: '#689F38'}} size={20} />
+							} />
 					}
 					rightComponent={
 						<HeaderButton onPress={() => this.props.navigation.navigate('UploadPhoto')} component={
@@ -18,11 +25,26 @@ export default class Feeds extends Component {
 						} />
 					} />
 
-				<ScrollView style={styles.content}>
-					<PhotoListItem onImagePress={() => this.props.navigation.navigate('Test')} />
-					<PhotoListItem />
-					<PhotoListItem />
-				</ScrollView>
+				<View style={styles.content}>
+					<FlatList ref={ref => this._feedList = ref} style={{flex: 1}} data={this.state.data}
+						keyExtractor={(item, index) => index.toString()}
+						renderItem={({item}) => (
+							<PhotoListItem onImagePress={() => this.props.navigation.navigate('Test')} />
+						)}
+						refreshing={this.state.refreshing}
+						onRefresh={() => {
+							this.setState({
+								refreshing: false,
+								data: [1, 2, 3]
+							});
+						}}
+						onEndReached={() => {
+							let newData = [...this.state.data];
+							newData.push(1);
+							newData.push(1);
+							this.setState({data: newData});
+						}} />
+				</View>
 			</View>
 		)
 	}
